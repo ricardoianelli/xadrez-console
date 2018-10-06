29ce4f145d0933d88d93ea6d5adc84c0bb149d8a
Ricardo Ianelli <ricardoianelli@hotmail.com>
2018-10-06 00:10:23 -0300

    Criando a classe posicao.

---
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return "Linha: "
+                + linha
+                + ", Coluna: "
+                + coluna;
+        }
+    }
+}
